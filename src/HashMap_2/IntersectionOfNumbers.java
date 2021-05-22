4315baae36f55224b32dc29dde1dbe384f178e95
Gaurav Kabra <gauravkabra.official@gmail.com>
2021-05-22 20:32:46 +0530

    Added alternative solution

---
+++ src/HashMap_2/IntersectionOfNumbers.java
@@ -0,0 +1,110 @@
+/**
+ * Google
+ * Given two integer arrays, return their intersection.
+ * Note: the intersection is the set of elements that are common to both arrays.
+ */
+package HashMap_2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author gaurav kabra
+ * @since May 21, 2021
+ */
+public class IntersectionOfNumbers {
+	
+	/**
+	 * @param arr1
+	 * @param arr2
+	 * @return intersection of arr1 & arr2 with distinct elements
+	 * Time: O(N) since we traverse arr1 and arr2 once
+	 * Space: O(N) since we store elements in sets and in worst case all elements are distinct
+	 */
+	private int[] getIntersectionBuiltInMethod(int[] arr1, int[] arr2) {
+		/**
+		 * Logic:
+		 * We just have to check element in first array that is also present in second array and return distinct elements.
+		 * This is nothing but interssection of sets.
+		 * In Java, retailAll method is used to do intersection.
+		 */
+		Set<Integer> st1 = new HashSet<>(),
+		st2 = new HashSet<>();
+		
+		int[] ans;
+		
+		for(int x : arr1)
+			st1.add(x);
+		for(int x : arr2)
+			st2.add(x);
+		/**
+		 * addAll() does Union
+		 * retailAll() does Intersection
+		 * containsAll() checks for subset
+		 * removeAll() takes set difference
+		 * (see: https://docs.oracle.com/javase/tutorial/collections/interfaces/set.html)
+		 */
+		st1.retainAll(st2);  // st1 now contains only elements in both sets (Retains only the elements in this set that are contained in arg set)
+		ans = new int[st1.size()];
+		
+		int i=0;
+		for(int x : st1) {
+			ans[i++] = x;
+		}
+		return ans;
+	}
+	
+	/**
+	 * @param arr1
+	 * @param arr2
+	 * @return intersection of arr1 & arr2 with distinct elements
+	 * Time: O(N) since we traverse arr1 and arr2 once
+	 * Space: O(N) since we store elements in sets and in worst case all elements are distinct
+	 */
+	private int[] getIntersection(int[] arr1, int[] arr2) {
+		Set<Integer> st = new HashSet<>();
+		Set<Integer> ans = new HashSet<>();
+		
+		for(int x : arr1)
+			st.add(x);
+		for(int x : arr2) {
+			if(st.contains(x))
+				ans.add(x);
+		}
+		Integer[] temp = ans.stream().toArray(Integer[]::new);
+		int[] ret = new int[temp.length];
+		
+		for(int i=0; i<temp.length; i++) {
+			ret[i] = temp[i];
+		}
+		return ret;
+	}
+	
+	private void displayAns(int[] ans) {
+		for(int x : ans)
+			System.out.print(x+" ");
+		System.out.println();
+	}
+	
+	// driver - main method
+	public static void main(String[] args) {
+		IntersectionOfNumbers ion = new IntersectionOfNumbers();
+		int[] ans;
+		// TESTCASES
+		ans = ion.getIntersection(new int[]{2, 4, 4, 2}, new int[]{2, 4});
+		ion.displayAns(ans);
+		ans = ion.getIntersection(new int[]{1, 2, 3, 3}, new int[]{3, 3});
+		ion.displayAns(ans);
+		ans = ion.getIntersection(new int[]{2, 4, 6, 8}, new int[]{1, 3, 5, 7});
+		ion.displayAns(ans);
+		
+		
+		ans = ion.getIntersectionBuiltInMethod(new int[]{2, 4, 4, 2}, new int[]{2, 4});
+		ion.displayAns(ans);
+		ans = ion.getIntersectionBuiltInMethod(new int[]{1, 2, 3, 3}, new int[]{3, 3});
+		ion.displayAns(ans);
+		ans = ion.getIntersectionBuiltInMethod(new int[]{2, 4, 6, 8}, new int[]{1, 3, 5, 7});
+		ion.displayAns(ans);
+	}
+
+}
--- src/Strings/CorrectCapitalization.java
+++ src/Strings/CorrectCapitalization.java
@@ -12,9 +12,31 @@ package Strings;
 public class CorrectCapitalization {
 	
 	/**
+	 * Check for correct capitalization Using regex
+	 * @param string s
+	 * @return boolean, if correctly capitalized
+	 * Time: Should be O(N). But the actual depends on Deterministic Finite Automaton (DFA) for regex
+	 * Space: O(1) since we require constant space
+	 */
+	private boolean isCorrectlyCapitalizedUsingRegex(String s) {
+		/**
+		 * Logic:
+		 * We are actually just doing patten matching - whether a given string matches a patten criterion
+		 * So we can use regex. We have 3 cases:
+		 * all small then [a-z]*
+		 * all capital then [A-Z]*
+		 * First letter capital and rest all small then [A-Z][a-z]*
+		 * If any one of the above three criteria matches then we return true. 
+		 */
+		return s.matches("[a-z]*|[A-Z]*|[A-Z][a-z]*");  // Note that there is no space among conditions
+	}
+	
+	/**
 	 * Check for correct capitalization
 	 * @param string s
 	 * @return boolean, if correctly capitalized
+	 * Time: O(N) as string comparison using equals method
+	 * Space: O(1) as we don't use any extra space
 	 */
 	private boolean isCorrectlyCapitalized(String s) {
 		/**
@@ -24,9 +46,6 @@ public class CorrectCapitalization {
 		 * 			We need to check for correct capitalization either remaining chars should be all caps or all small
 		 * Else:
 		 * 			remaining chars should be all small
-		 * 
-		 * Time: O(N) as string comparison using equals method
-		 * Space: O(1) as we don't use any extra space
 		 */
 		if(s == null || s.isEmpty())
 			return true;
@@ -63,6 +82,11 @@ public class CorrectCapitalization {
 		System.out.println(cc.isCorrectlyCapitalized("Calvin"));
 		System.out.println(cc.isCorrectlyCapitalized("compUter"));
 		System.out.println(cc.isCorrectlyCapitalized("coding"));
+		
+		System.out.println(cc.isCorrectlyCapitalizedUsingRegex("USA"));
+		System.out.println(cc.isCorrectlyCapitalizedUsingRegex("Calvin"));
+		System.out.println(cc.isCorrectlyCapitalizedUsingRegex("compUter"));
+		System.out.println(cc.isCorrectlyCapitalizedUsingRegex("coding"));
 	}
 
 }
