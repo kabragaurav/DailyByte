5aebd453d10f8e0b2e1303379a56125d4083fa93
Gaurav Kabra <gauravkabra.official@gmail.com>
2021-06-13 20:29:55 +0530

    june13

--- src/Trees/MinimumDifference.java
+++ src/Trees/MinimumDifference.java
@@ -7,9 +7,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import Trees.TreeUtils.TreeNode;
-
+/**
+ * @author gaurav kabra
+ * @since June 12, 2021
+ */
 public class MinimumDifference {
 	
+	// list that will hold values of all nodes
     List<Integer> ls = new ArrayList<>();
     
     private void traverse(TreeNode<Integer> root) {
@@ -18,11 +22,22 @@ public class MinimumDifference {
         traverse(root.left);
         ls.add(root.val);
         traverse(root.right);
-        
     }
     
     
     public int getMinimumDifference(TreeNode<Integer> root) {
+    	/**
+    	 * Logic:
+    	 * Before finding the difference, we first need to traverse the tree.
+    	 * So we traverse the tree and store value of each node in the list, ls.
+    	 * We sort the list in end (No need if traverse is inorder, since then list is already sorted).
+    	 * Then to find minimum difference, we just track differences of adjacent values and return minimum of them.
+    	 * 
+    	 * Time Complexity : O(N) for we traverse the tree (which can be skewed in worst case) + we trace the list to find differences.
+    	 * Note that it will be O(NlogN) if your traversal is not inorder (since then you will need to sort as well)
+    	 * 
+    	 * Space Complexity : O(N) for internal stack in recursion involved in traversal and list to store all values present in tree.
+    	 */
         traverse(root);
         
         int mini = Integer.MAX_VALUE, diff;
@@ -31,6 +46,7 @@ public class MinimumDifference {
             diff = ls.get(i+1) - ls.get(i);
             mini = Math.min(mini, diff);
         }
+        
         return mini;
     }
     
@@ -39,7 +55,8 @@ public class MinimumDifference {
  	    TreeNode<Integer> treeNode = new TreeNode<Integer>(val);
  		return treeNode;
  	}
-
+ 	
+ 	// driver - main method
 	public static void main(String[] args) {
 		
 		// make Tree
@@ -62,6 +79,7 @@ public class MinimumDifference {
 		four.left = three;
 		four.right = five;	
 		
+		// TESTCASE
 		MinimumDifference md = new MinimumDifference();
 		System.out.println(md.getMinimumDifference(root));
 
---
+++ src/Trees/ModeInTree.java
@@ -0,0 +1,115 @@
+/**
+ * Given a binary search tree, return all modes.
+ * Mode is the most frequently occurring value.
+ */
+package Trees;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import Trees.TreeUtils.TreeNode;
+
+/**
+ * @author gaurav kabra
+ * @since June 13, 2021
+ */
+public class ModeInTree {
+    
+	// stores the magnitude of mode (E.g. if mode is 4 that occurs 2 times, then it stores 2)
+    int modeMagnitude;
+    // tracks number of values that occur modeMagnitude times
+    int count;
+    
+    private void inOrder(TreeNode<Integer> root, Map<Integer, Integer> mp) {
+        if(root == null)
+            return;
+        inOrder(root.left, mp);
+        mp.put(root.val, mp.getOrDefault(root.val, 0) + 1);
+        if(mp.get(root.val) > modeMagnitude) {
+            modeMagnitude = mp.get(root.val);
+            count = 0;
+        }
+        else if(mp.get(root.val) == modeMagnitude) {
+            count++;
+        }
+        inOrder(root.right, mp);
+    }
+	
+	
+	private int[] findMode(TreeNode<Integer> root) {
+		/**
+		 * Time Complexity: O(N), where N is the number of nodes in the BST (We have to visit each node).
+         * Space Complexity: O(N) (maximum amount of space utilized by the recursion stack in traversal would be N since the height of a skewed BST could be N).
+         */
+		
+		// initialize here, so that values reset for each call
+		modeMagnitude= Integer.MIN_VALUE;
+		count = 0;
+		
+		// map to store frequency of each node
+        Map<Integer, Integer> mp = new HashMap<>();
+        
+        // traverse the tree
+        inOrder(root, mp);
+        
+        // array that stores node values that occur maximum in tree
+        int[] arr = new int[count+1];
+        int i = 0;
+        
+        for(Map.Entry<Integer, Integer> entry : mp.entrySet()) {
+            if(entry.getValue() == modeMagnitude) {
+                arr[i++] = entry.getKey();
+            }
+        }
+        
+        return arr;
+        
+    }
+	
+	// helper method to main
+ 	private static TreeNode<Integer> getTreeNode(Integer val) {
+ 	    TreeNode<Integer> treeNode = new TreeNode<Integer>(val);
+ 		return treeNode;
+ 	}
+ 	
+ 	// driver - main method
+	public static void main(String[] args) {
+		
+		// make Tree
+	    TreeNode<Integer> root = getTreeNode(6);
+		TreeNode<Integer> two = getTreeNode(2);
+		TreeNode<Integer> eight = getTreeNode(8);
+		TreeNode<Integer> zero = getTreeNode(0);
+		TreeNode<Integer> four_1 = getTreeNode(4);
+		TreeNode<Integer> seven = getTreeNode(7);
+		TreeNode<Integer> nine = getTreeNode(9);
+		TreeNode<Integer> four_2 = getTreeNode(4);
+		TreeNode<Integer> five = getTreeNode(5);
+		TreeNode<Integer> three = getTreeNode(3);
+				
+		root.left = two;	
+		root.right = eight;
+		two.left = zero;
+		two.right = four_1;
+		eight.left = seven;
+		eight.right = nine;
+		four_1.left = four_2;
+		four_1.right = five;	
+		
+		
+		ModeInTree mit = new ModeInTree();
+		
+		// TESTCASES
+		int[] arr = mit.findMode(root);
+		System.out.println(Arrays.toString(arr));
+		
+		four_1.left = three;
+		arr = mit.findMode(root);
+		System.out.println(Arrays.toString(arr));
+		
+		arr = mit.findMode(null);
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
